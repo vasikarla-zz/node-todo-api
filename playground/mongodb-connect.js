2c10c41511399fcfdced48dcb3ccc157265f9320
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-08 16:47:10 -0700

    bot commit message

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,12 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+var options = { useNewUrlParser: true };
+MongoClient.connect('mongodb://localhost:27017/TodoApp', options, (err, db) => {
     if (err) {
         return console.log("Unable to connect to Mongodb Server")
     } 
 
-    console.log("Connection Sucessful...");
+    console.log("Connection Sucessful....");
 
     db.close();
+    console.log("Connection Closed....");
 
 });
