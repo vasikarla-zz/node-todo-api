aabc286265d258999327fa61eb468bede01028f4
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-12 15:58:12 -0700

    automatic commit message

--- server/server.js
+++ server/server.js
@@ -1,18 +1,61 @@
 var mongoose = require('mongoose');
+var faker = require('faker');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-const Todo = mongoose.model('ToDo', {
+const Todo = mongoose.model('ToDos', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false    
     },
 
     completedAt: {
-        type: Number
+        type: Number,
+        default: null        
     }
 });
+
+const User = mongoose.model('Users', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+//Creating a new ToDo
+var newTodo = new Todo({
+    text: "     Raj     ",
+    completed: true,
+    completedAt: 12345
+});
+
+//Creating a user
+var user = new User({
+    email: faker.internet.email()
+});
+user.save().then((res, err) => {
+    if (err) {
+        console.log("This is error :" + err);
+    }
+    console.log(JSON.stringify(res, undefined, 2));
+});
+
+newTodo.save().then((err, res) => {
+    if (err) {
+        console.log("This is error :" + err);
+    }
+    console.log(JSON.stringify(res, undefined, 2));
+});
+
+
+
