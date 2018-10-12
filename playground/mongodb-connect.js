93b477a1c0390663bdf186da63620a86b0e585b2
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-11 22:14:42 -0700

    fixing mongo db connection issue

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,13 +1,20 @@
-const MongoClient = require('mongodb').MongoClient;
-var options = { useNewUrlParser: true };
-MongoClient.connect('mongodb://localhost:27017/TodoApp', options, (err, db) => {
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db("TodoApp");
+  
+  db.collection("Todos").insertOne({
+    text: "Sample-Text",
+    completed: false
+  }, (err, result) => {
     if (err) {
-        return console.log("Unable to connect to Mongodb Server")
-    } 
-
-    console.log("Connection Sucessful....");
-
-    db.close();
-    console.log("Connection Closed....");
-
+      console.log("Unable to insert " + err);
+    }
+    JSON.stringify(result, undefined, 2);
+  });
+  client.close();
 });
