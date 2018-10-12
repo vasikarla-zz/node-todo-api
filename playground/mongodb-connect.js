53417efcc76f773a3fb042d8851562efe13f0d68
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-11 22:47:09 -0700

    automatic commit message

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,20 +1,24 @@
-var MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+var {faker} = require('faker');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
+  
   console.log('Connected to MongoDB server');
   const db = client.db("TodoApp");
   
-  db.collection("Todos").insertOne({
-    text: "Sample-Text",
-    completed: false
-  }, (err, result) => {
-    if (err) {
-      console.log("Unable to insert " + err);
-    }
-    JSON.stringify(result, undefined, 2);
-  });
+  //INSERT DATA INTO MONGODB
+  // db.collection("Users").insertOne({
+  //   name: faker.name.findName(),
+  //   age: 38,
+  //   locations: "Pleasanton"
+  // }, (err, res) => {
+  //   if (err) {
+  //     console.log(`Error Occured ${err}`);
+  //   }
+  //   console.log(JSON.stringify(res.ops[0]._id.getTimestamp(), undefined, 3));
+  // })
   client.close();
 });
