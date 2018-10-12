98ec46a862cc1fbc69b5fedb217c3076b796f20a
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-12 12:43:19 -0700

    automatic commit message

---
+++ playground/mongodb-update.js
@@ -0,0 +1,44 @@
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+var {
+  faker
+} = require('faker');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+  const db = client.db("TodoApp");
+
+  //DELETE MANY
+  // db.collection("Users").deleteMany({name: "Raj"}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //DELETE ONE
+  // db.collection("Users").deleteOne({text: "5bc02e905fbaa40e4ce51c4c"}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //FIND-ONE & UPDATE
+  db.collection("Users").findOneAndUpdate({
+    name: "Deshaun DuBuque"
+  }, {
+    $set: {
+      name: "Sasha Vasikarla"
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //FETCH COUNT
+
+
+  // client.close();
+});
