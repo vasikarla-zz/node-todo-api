b96f6e883e2a8e103b292b22758df96324570fa0
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-13 12:11:40 -0700

    automatic commit message

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,41 @@
-var {mongoose} = require('../db/mongoose');
+const {mongoose} = require('../db/mongoose');
+const validator = require('validator');
+// {
+//     "   ": "vasikarla.raj@gmail.com",
+//     "password": "splendor",
+//     "tokens": [{
+//         access: "auth",
+//         token: "splendor-token"
+//     }]
+// };
 
 const User = mongoose.model('Users', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        uniquie: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports =  User;
