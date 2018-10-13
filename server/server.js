507950007d7e317441835677941e4153bcf006d7
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-13 09:21:21 -0700

    automatic commit message

--- server/server.js
+++ server/server.js
@@ -49,8 +49,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
