5f14df2d1294aed2cc5583990e7b31d36e1d76d9
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-14 19:10:47 -0700

    automatic commit message

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const {mongoose} = require('../db/mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -30,7 +31,25 @@ var UserSchema = new mongoose.Schema({
     }]
 }); 
 
-UserSchema.methods.generateAuthToken
+UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+
+    var token = jwt.sign({
+        _id: user._id.toHexString(), 
+        access: access
+    }, 'abc123').toString();
+
+    // user.tokens.push({
+    //     access,
+    //     tokens
+    // });
+
+    user.tokens = user.tokens.concat([{access, token}]);
+    user.save().then(() => {
+        return token;
+    });
+};
 
 const User = mongoose.model('Users', UserSchema);
 
